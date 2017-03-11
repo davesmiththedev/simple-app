c88e3d3ab17fb7fec685e0cab0e07006f7fa08ff
davidleroy-js <davidleroy.js@gmail.com>
2017-03-11 15:57:09 -0800

    added accounts and submit post features

--- client/templates/posts/post_item.js
+++ client/templates/posts/post_item.js
@@ -2,6 +2,7 @@ Template.postItem.helpers({
     domain: function() {
         var a = document.createElement('a');
         a.href = this.url;
+        Session.set('pageTitle', 'Poop');
         return a.hostname;
     }
 });
