5ebcb7ae79076760affc77ed64b883eda16e4b24
davidleroy-js <davidleroy.js@gmail.com>
2017-03-12 13:09:26 -0700

    added functions to update form -cancel button -duplicate link check

--- client/templates/posts/post_edit.js
+++ client/templates/posts/post_edit.js
@@ -2,19 +2,29 @@ Template.postEdit.events({
     'submit form': function(e){
         e.preventDefault();
         
-        var currentPostId = this.id;
+        var currentPostId = this._id;
 
         var postProperties = {
             url: $(e.target).find('[name=url]').val(),
             title: $(e.target).find('[name=title]').val()
         }
 
-        Posts.update(currentPostId, {$set: postProperties}, function(error){
+        Meteor.call('postUpdate', postProperties, function (error, result){
             if(error){
-                alert(error.reason);
-            }else{
-                Router.go('postPage', {_id: currentPostId});
+                return alert(error.reason);
             }
+
+            if(result.postExists){
+                return alert('This link has already been posted');
+            }
+
+            Posts.update(currentPostId, {$set: postProperties}, function(error){
+                if(error){
+                    alert(error.reason);
+                }else{
+                    Router.go('postPage', {_id: currentPostId});
+                }
+            });
         });
     },
 
